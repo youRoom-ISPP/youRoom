8b4d3a30df144b557d0992376547412a87a17ebc
ferruirob <ferruirob@alum.us.es>
2021-03-19 20:08:11 +0100

    Cambios a view-#9
    Solucionado error en visualizacion vista para confirmacion de creacion de publicacion
    y creada la clase PublicacionView

--- youroom/publicacion/urls.py
+++ youroom/publicacion/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from publicacion import views
 
 urlpatterns = [
-    path('form_publicacion/', views.create_formulario_publicacion),
-    path('publicacion_realizada/', views.publicado_exito)
+    path('form_publicacion/', views.PublicacionView.create_formulario_publicacion),
+    path('publicacion_realizada/', views.PublicacionView.publicado_exito)
 ]
--- youroom/publicacion/views.py
+++ youroom/publicacion/views.py
@@ -2,50 +2,44 @@ import time
 from datetime import datetime
 from django.urls import reverse
 from publicacion.models import Publicacion
-from django.shortcuts import render, HttpResponseRedirect, redirect
+from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
 from publicacion.forms import FormularioPublicacion
 
-# Create your views here.
-
-def create_formulario_publicacion(request):
-    if request.method == 'POST':
-        create_publicacion(request)
-
-    form = FormularioPublicacion()
-    return render(request, 'publicacion/form_publicacion.html', {'form':form})
-
-
-def create_publicacion(request):
-    form = FormularioPublicacion(request.POST)
-
+class PublicacionView(TemplateView):
     
-
-    if form.is_valid():
-        errors = []
-        descripcion = form.cleaned_data.get('descripcion')
-
-        if not descripcion:
-            errors.append('La descripción no puede estar vacía')
-            return render(request, 'publicacion/form_publicacion.html', {'form':form, 'errors':errors})
-
-        categoria = form.cleaned_data.get('categoria')
-        hora_actual = datetime.now()
-
-        publicacion = Publicacion.objects.get_or_create(
-            descripcion=descripcion,
-            categoria=categoria,
-            fecha_publicacion=hora_actual
-        )
-
-        return redirect('/publicacion_realizada/')
-        
+    def create_formulario_publicacion(request):
+        if request.method == 'POST':
+            PublicacionView.create_publicacion(request)
+            return redirect('/publicacion_realizada/')
+        else:
+            form = FormularioPublicacion()
+            return render(request, 'publicacion/form_publicacion.html', {'form':form})
 
 
-    else:
-        print('hola')
-        return render(request, 'publicacion/form_publicacion.html', {'form':form})
+    def create_publicacion(request):
+        form = FormularioPublicacion(request.POST)
+        if form.is_valid():
+            errors = []
+            descripcion = form.cleaned_data.get('descripcion')
 
-def publicado_exito(request):
-    return render(request, 'publicacion/exito.html')
+            if not descripcion:
+                errors.append('La descripción no puede estar vacía')
+                return render(request, 'publicacion/form_publicacion.html', {'form':form, 'errors':errors})
+
+            categoria = form.cleaned_data.get('categoria')
+            hora_actual = datetime.now()
+
+            publicacion = Publicacion.objects.get_or_create(
+                descripcion=descripcion,
+                categoria=categoria,
+                fecha_publicacion=hora_actual
+            )
+            
+        else:
+            return render(request, 'publicacion/form_publicacion.html', {'form':form})
+
+    def publicado_exito(request):
+        return render(request, 'publicacion/exito.html')
 
 
