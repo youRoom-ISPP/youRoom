30ae2d044e28eed03c8e277d737d87303b363dde
rafsalcas1 <rafsalcas1@rafasc27.localdomain>
2021-03-23 18:51:06 +0100

    Adicción de listar publicaciones destacadas y por categorias-#10

--- youroom/timeline/views.py
+++ youroom/timeline/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from publicacion.models import Publicacion
+from publicacion.models import Publicacion, Destacada
+from publicacion.enum import Categorias
 
+
+class TimelineViewCategorias(TemplateView):
+    template_name = 'timeline/timeline.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs.get('pk')
+        context['publicaciones'] = Publicacion.objects.filter(categoria=pk)
+        return context
 
 class TimelineView(TemplateView):
     template_name = 'timeline/timeline.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['publicaciones'] = Publicacion.objects.all()
+        res = Destacada.objects.all()
+        publicaciones = []
+        for destacada in res:
+            publicaciones.append(destacada.publicacion)
+        context['publicaciones'] = publicaciones
         return context
