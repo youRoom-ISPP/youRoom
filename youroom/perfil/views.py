81a81fb81117c3af2bce3a5d6cea9dce233952e2
rafsalcas1 <rafsalcas1@alum.us.es>
2021-03-24 19:57:02 +0100

    Adicción de perfil usuario-#11

--- youroom/perfil/views.py
+++ youroom/perfil/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from publicacion.models import Publicacion
+from usuario.models import UsuarioPerfil
+
 
 class PerfilView(TemplateView):
     template_name = 'perfil/perfil.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        usuario, create = UsuarioPerfil.objects.get_or_create(user = self.request.user)
+        context['publicaciones'] = Publicacion.objects.filter(usuario=usuario)
+        context['user'] = usuario
+        return context
