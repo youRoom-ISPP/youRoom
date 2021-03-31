c712b74f285289d7b0610dea1da8e6f86024d010
alfgonfer <alfgonfer@alum.us.es>
2021-03-31 23:10:08 +0200

    Realizado eliminación de destacadas tras 1 día-#71
    Ahora los publicaciones destacadas que tengan más de un "día de vida" se eliminan de la clase Destacada, haciendo que no aparezcan con las otras destacadas al principio de la vista de timeline.

--- youroom/publicacion/admin.py
+++ youroom/publicacion/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Publicacion, Etiqueta
+from .models import Publicacion, Etiqueta, Destacada
 
 admin.site.register(Publicacion)
 admin.site.register(Etiqueta)
+admin.site.register(Destacada)
--- youroom/ranking/admin.py
+++ youroom/ranking/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Valoracion
+admin.site.register(Valoracion)
 # Register your models here.
