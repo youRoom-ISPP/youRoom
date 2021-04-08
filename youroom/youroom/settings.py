958ff2f54ee38093ce102f4c2ae5e7b13393e0d4
ferruirob <ferruirob@alum.us.es>
2021-04-08 20:31:01 +0200

    PC-#110 Integrado ejemplo pago producto

--- youroom/youroom/settings.py
+++ youroom/youroom/settings.py
@@ -165,3 +165,6 @@ if os.getenv('PROD') == 'True':
     AWS_S3_HOST = 'youroom.s3.amazonaws.com'
     AWS_S3_REGION_NAME = 'eu-west-3'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STRIPE_PUBLIC_KEY = ""
+STRIPE_SECRET_KEY = ""
+STRIPE_WEBHOOK_SECRET = ""
