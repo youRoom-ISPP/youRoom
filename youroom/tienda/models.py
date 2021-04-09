136ffda226ee054d534bdb96b34d28868f19a004
ferruirob <ferruirob@alum.us.es>
2021-04-09 10:41:51 +0200

    Eliminando html y código reciiclado de pago

--- youroom/tienda/models.py
+++ youroom/tienda/models.py
@@ -4,8 +4,6 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)  # cents
-    file = models.FileField(upload_to="product_files/", blank=True, null=True)
-    url = models.URLField()
 
     def __str__(self):
         return self.name
--- youroom/tienda/urls.py
+++ youroom/tienda/urls.py
@@ -11,11 +11,5 @@ from tienda.views import (
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('create-payment-intent/<pk>/', StripeIntentView.as_view(), name='create-payment-intent'),
-    path('webhooks/stripe/', stripe_webhook, name='stripe-webhook'),
-    path('cancel/', CancelView.as_view(), name='cancel'),
-    path('success/', SuccessView.as_view(), name='success'),
-    path('', ProductLandingPageView.as_view(), name='landing-page'),
-    path('create-checkout-session/<pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+   
 ]
