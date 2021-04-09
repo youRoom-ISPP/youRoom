57c5d9cade421d04dbffbc1bfad2c53dd36e2eab
jorandmol <jorandmol@alum.us.es>
2021-04-09 18:03:20 +0200

    Finalización de la interfaz de registro - #98
    He concluido la interfaz y sus validaciones.

---
+++ youroom/static/js/registro.js
@@ -0,0 +1,29 @@
+//Comprobar que la contraseña es la misma
+function coincidePass() {
+    var pass = $("#password-reg").val();
+    var confirmPass = $("#password-confirm-reg").val();
+    if (pass != confirmPass) {
+        $("#password-confirm-reg").css("background-color","#f8d7da");
+        $("#boton-registro").attr("disabled", true);
+    } else {
+        $("#password-confirm-reg").css("background-color","#78f8da");
+        $("#boton-registro").attr("disabled", false);
+    }
+}
+
+//Comprobar que la contraseña tenga más de 8 caracteres
+function longPass() {
+    var pass = $("#password-reg").val();
+    if (pass.length < 8) {
+        $("#password-reg").css("background-color","#f8d7da");
+        $("#boton-registro").attr("disabled", true);
+    } else {
+        $("#password-reg").css("background-color","#78f8da");
+        $("#boton-registro").attr("disabled", false);
+    }
+}
+
+$(document).ready(function(){
+    $("#password-confirm-reg").keyup(coincidePass);
+    $("#password-reg").keyup(longPass);
+});
