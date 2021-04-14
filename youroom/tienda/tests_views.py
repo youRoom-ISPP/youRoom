15dd5342ab93e842087de31a6861649828f53ee9
Javier <javsolgar@alum.us.es>
2021-04-15 00:20:32 +0200

    Se ha refactorizado código y se ha añadido un test-#122
    Se ha refactorizado el código y se ha añadido un test para comprobar que un usuario no autenticado no pueda acceder al proceso de compra

--- youroom/tienda/tests_views.py
+++ youroom/tienda/tests_views.py
@@ -1,5 +1,6 @@
 import io, os
 from PIL import Image
+from django.core.management import call_command
 from rest_framework.test import  APIClient , APITestCase
 from django.contrib.auth.models import User
 from usuario.models import UsuarioPerfil, ContadorVida, Premium
@@ -17,14 +18,7 @@ class TiendaViewTest(APITestCase):
         self.u.save()
         self.p = UsuarioPerfil.objects.get_or_create(user = self.u,totalPuntos=100)[0]
         self.c= ContadorVida.objects.get_or_create(perfil=self.p,estaActivo=True)[0]
-        self.u2 = User(username='prueba3')
-        self.u2.set_password('usuario1234')
-        self.u2.email = 'prueba3@gmail.com'
-        self.u2.isActive=True
-        self.u2.save()
-        self.p2 = UsuarioPerfil.objects.get_or_create(user = self.u2,totalPuntos=100,id_stripe="cus_JIUr31c2MApWC6")[0]
-        self.c2= ContadorVida.objects.get_or_create(perfil=self.p2,estaActivo=True)[0]
-        self.suscripcion = Product(name="suscripcion",price=399,numVidas=0)
+        call_command('loaddata', 'products.json', verbosity=0)
 
 
     def tearDown(self):
@@ -34,29 +28,35 @@ class TiendaViewTest(APITestCase):
         response = self.client.get("http://testserver{}".format(reverse("home")))
         self.assertEqual(response.status_code, 302)
         self.assertTemplateUsed(template_name='usuario/login.html')
-    
+
+    def test_charge_view_not_logged(self):
+        response = self.client.get('/tienda/charge/1')
+        self.assertEqual(response.status_code, 301)
+        self.assertTemplateUsed(template_name='usuario/login.html')
+
     def test_tienda_view(self):
-        answers = {
-            'username': 'prueba',
-            'password': 'usuario1234'
-        }
-        login = self.client.post('', answers)
+        self.client.login(username='prueba', password='usuario1234')
+
         response = self.client.get("http://testserver{}".format(reverse("home")))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(template_name='tienda/home.html')
 
     def test_cancelar_premium_logged(self):
 
-        answers = {
-            'username': 'prueba3',
-            'password': 'usuario1234'
-        }
-        login = self.client.post('', answers)
+        self.client.login(username='prueba', password='usuario1234')
     
-        prem = Premium.objects.get_or_create(perfil=self.p2)[0]
-        fechaCancelacion = prem.fechaCancelacion
+        prem = Premium.objects.get_or_create(perfil=self.p)[0]
+        perfil = UsuarioPerfil.objects.get(user=self.u)
+        perfil.id_stripe = 'cus_JIUr31c2MApWC6'
+        perfil.save()
+        self.assertIsNone(prem.fechaCancelacion)
         
         response = self.client.get("http://testserver{}".format(reverse("cancel")))
+        prem = Premium.objects.get_or_create(perfil=self.p)[0]
+
         self.assertEqual(response.status_code, 302)
+        self.assertIsNotNone(prem.fechaCancelacion)
         self.assertTemplateUsed(template_name='perfil/perfil.html')
+
+
 
